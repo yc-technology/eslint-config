498d8c0e3c0f2ba59e2c8139f241e2c9786ddfc1
django <six.django@gmail.com>
2024-02-22 23:03:42 +0800

    feat: init

---
+++ index.js
@@ -0,0 +1,23 @@
+module.exports = {
+    extends: [
+      'eslint:recommended',
+      'plugin:@typescript-eslint/recommended',
+      'plugin:react-hooks/recommended',
+      'prettier'
+    ],
+    ignorePatterns: ['dist', '.eslintrc.cjs'],
+    parser: '@typescript-eslint/parser',
+    plugins: ['react-refresh'],
+    rules: {
+      'react-refresh/only-export-components': 'off',
+      'no-unused-vars': 'off',
+      'no-undef': 'off',
+      // 关闭!非空断言警告
+      'react-hooks/rules-of-hooks': 'off',
+      '@typescript-eslint/no-non-null-assertion': 'off',
+      '@typescript-eslint/no-empty-function': 'off',
+      'react-hooks/exhaustive-deps': 'off',
+      '@typescript-eslint/ban-types': 'off'
+    }
+  }
+  
